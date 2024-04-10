b1254a44fb3447ebd71ec113a94ca759aefe573c
ozzux <osama.radi7@gmail.com>
2024-04-10 18:07:26 +0300

    edited model

--- training.py
+++ training.py
@@ -5,14 +5,17 @@ model = tf.keras.Sequential()
 
 model.add(tf.keras.layers.InputLayer((23000,)))
 model.add(tf.keras.layers.Activation(tf.nn.relu))
-model.add(tf.keras.layers.Dense(400, activation=tf.nn.relu))
-model.add(tf.keras.layers.Dense(400, activation=tf.nn.relu))
-model.add(tf.keras.layers.Dense(400, activation=tf.nn.relu))
-model.add(tf.keras.layers.Dense(400, activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(1000, activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(1000, activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(1000, activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(1000, activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(1000, activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(1000, activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(1000, activation=tf.nn.relu))
 model.add(tf.keras.layers.Dense(1, activation=tf.nn.sigmoid))
 
-model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
-model.fit(x_train, y_train, epochs=5)
+model.compile(optimizer="adam", loss=tf.losses.binary_crossentropy, metrics=["accuracy"])
+model.fit(x_train, y_train, epochs=10)
 
 val_loss, val_acc = model.evaluate(x_test,y_test)
 
