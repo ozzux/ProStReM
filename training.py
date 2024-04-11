f18af2f3c04b9f0f199fe6837b4e93496cca4073
ozzux <osama.radi7@gmail.com>
2024-04-11 09:29:08 +0300

    created models

--- training.py
+++ training.py
@@ -8,21 +8,18 @@ while True:
     model = tf.keras.Sequential()
 
     model.add(tf.keras.layers.InputLayer((23000,)))
-    model.add(tf.keras.layers.Activation(tf.nn.relu))
-    model.add(tf.keras.layers.Dense(500, activation=tf.nn.relu))
-    model.add(tf.keras.layers.Dense(500, activation=tf.nn.relu))
-    model.add(tf.keras.layers.Dense(500, activation=tf.nn.relu))
-    model.add(tf.keras.layers.Dense(500, activation=tf.nn.relu))
-    model.add(tf.keras.layers.Dense(500, activation=tf.nn.relu))
-    model.add(tf.keras.layers.Dense(500, activation=tf.nn.relu))
-    model.add(tf.keras.layers.Dense(500, activation=tf.nn.relu))
+    model.add(tf.keras.layers.Dense(1000, activation=tf.nn.relu))
+    model.add(tf.keras.layers.Dense(1000, activation=tf.nn.relu))
+    model.add(tf.keras.layers.Dense(1000, activation=tf.nn.relu))
+    model.add(tf.keras.layers.Dense(1000, activation=tf.nn.relu))
+    model.add(tf.keras.layers.Dense(1000, activation=tf.nn.relu))
     model.add(tf.keras.layers.Dense(1, activation=tf.nn.sigmoid))
 
     model.compile(optimizer="adam", loss=tf.losses.binary_crossentropy, metrics=["accuracy"])
     model.fit(x_train, y_train, epochs=10)
     val_loss, val_acc = model.evaluate(x_test,y_test)
     print(val_acc)
-    if val_acc >= 0.8:
+    if val_acc >= 0.85:
         break
 
 print(val_acc)
